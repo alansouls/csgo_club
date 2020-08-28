3bd20705100a1cbe6bda0982fc8fc3f388a61056
Alan <maiaalan@alu.ufc.br>
2020-08-28 00:52:56 -0300

    Add starting server proof of concept

---
+++ proof_of_concept/csgo_creator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace csgo_creator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                using (Process p = new Process())
+                {
+                    p.StartInfo.UseShellExecute = false;
+                    // You can start any process, HelloWorld is a do-nothing example.
+                    p.StartInfo.FileName = "/bin/bash";
+                    p.StartInfo.Arguments = "commands/exec_server.sh";
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.RedirectStandardInput = true;
+                    p.Start();
+                    Exit(p);
+                    p.WaitForExit();
+                }
+            }
+            catch (Exception e){
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static async Task Exit(Process p) 
+        {
+            await Task.Delay(5000);
+            p.StandardInput.WriteLine("exit");
+        }
+    }
+}
---
+++ proof_of_concept/csgo_creator/commands/exec_server.sh
@@ -0,0 +1,1 @@
+/home/alan/.steam/steamcmd/csgo/srcds_run -game csgo -console -usercon +game_type 0 +game_mode 1 +mapgroup mg_active +map de_dust2
