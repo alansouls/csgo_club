0e9a04e2669cb11bed544985ffaf09f186af2610
alanAval <alan@avalengenharia.eng.br>
2020-09-19 23:10:23 -0300

    Integrate blob service

---
+++ src/BlobService/Entity/BlobFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlobServices.Entity
+{
+    /// <summary>
+    /// Estrutura para guardar informações de um arquivo salvo no Blob.
+    /// </summary>
+    public class BlobFile
+    {
+        /// <summary>
+        /// Nome completo do arquivo: pasta/arquivo
+        /// </summary>
+        public string FullName { get; }
+        /// <summary>
+        /// Nome da pasta do arquivo, ou caminho de pastas: pasta ou pasta1/pasta2
+        /// </summary>
+        public string Folder { get; set; }
+        /// <summary>
+        /// Nome apenas do arquivo: arquivo.bin
+        /// </summary>
+        public string Name { get;}
+        /// <summary>
+        /// Uri para recuperação do arquivo
+        /// </summary>
+        public Uri Uri { get;}
+
+        public BlobFile(Uri blobUri)
+        {
+            Name = ExtractName(blobUri);
+            Uri = blobUri;
+            Folder = ExtractFolder(blobUri);
+            FullName = Folder + "/" + Name;
+        }
+
+        private string ExtractName(Uri uri)
+        {
+            var name = uri.LocalPath.Split("/").Last();
+            return name;
+        }
+
+        private string ExtractFolder(Uri uri)
+        {
+            var paths = uri.LocalPath.Split("/");
+            return string.Join("/", paths.Take(paths.Count() - 1).ToList());
+        }
+    }
+}
---
+++ src/BlobService/Service/IBlobService.cs
@@ -0,0 +1,42 @@
+﻿using BlobServices.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlobServices.Services
+{
+    public interface IBlobService
+    {
+        /// <summary>
+        /// Ativa um blob container com o nome passado no parâmetro container.
+        /// </summary>
+        /// <param name="container"></param>
+        void SelectBlobContainer(string container);
+
+        /// <summary>
+        /// Faz upload de um arquivo esalva ele com o nome especificado em fileName.
+        /// </summary>
+        /// <param name="file">Byte Array do arquivo.</param>
+        /// <param name="fileName">Nome do arquivo para recuperar depois.</param>
+        void UploadFile(byte[] file, string fileName);
+
+        /// <summary>
+        /// Recupera o arquivo em um byte array atráves do seu caminho.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        byte[] RetrieveFile(string path);
+
+        /// <summary>
+        /// Retorna uma string listando todos os arquivos do container blob ativo.
+        /// </summary>
+        /// <returns></returns>
+        string ListFilesString();
+
+        /// <summary>
+        /// Recupera a lista de arquivos como objeto BlobFile do container blob ativo
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<BlobFile> ListFiles();
+    }
+}
