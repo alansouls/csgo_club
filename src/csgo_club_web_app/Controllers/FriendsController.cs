5984795b0ced8165cfcbe6a053978b92c16147e2
eduardoaval <eduardo@avalengenharia.eng.br>
2020-10-15 22:14:55 -0300

    Add friend

--- src/csgo_club_web_app/Controllers/FriendsController.cs
+++ src/csgo_club_web_app/Controllers/FriendsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using csgo_club_web_app.Models;
 using CsgoClubEF.Entities;
 using CsgoClubEF.Repository;
 using Microsoft.AspNetCore.Http;
@@ -28,6 +29,44 @@ namespace csgo_club_web_app.Controllers
             if (friends.Count() == 0)
                 return View("NotFound");
             return View(user);
+        }
+
+        public IActionResult Users([FromQuery] string name)
+        {
+            var users = _unityOfWork.GetRepository<User>().Query(x => x.Name.Contains(name)).ToList();
+            return View(users);
+        }
+
+        public IActionResult Add([FromRoute] Guid id)
+        {
+            var steamId = UInt64.Parse(User.Claims.First().Value.Split("id/")[2]);
+            var user = _unityOfWork.GetRepository<User>().Query(x => x.SteamId == steamId)
+                .Include(x=> x.Friends).ThenInclude(x=> x.Friend).FirstOrDefault();
+            if(user.Friends.ToList().Find(x=> x.FriendId == id) != null)
+                return Redirect(Url.Action("Index"));
+            _unityOfWork.GetRepository<FriendList>().Add(new FriendList()
+                {
+                    UserId = user.Id,
+                    FriendId = id
+                });
+            _unityOfWork.Save();
+            return Redirect(Url.Action("Users"));
+        }
+
+        public IActionResult Search() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Search(SearchModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return Redirect(Url.Action("Users", new { name = model.Name }));
+            }
+            return Redirect(Url.Action("Search"));
         }
     }
 }
---
+++ src/csgo_club_web_app/Models/SearchModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csgo_club_web_app.Models
+{
+    public class SearchModel
+    {
+        public string Name { get; set; }
+    }
+}
